f7554b7e0b06c609a6652f43d82faf9d41051a2f
slowy07 <slowy.arfy@proton.me>
2024-11-14 16:06:23 +0700

    chore: update multiboot reference from boot.asm
    Signed-off-by: slowy07 <slowy.arfy@proton.me>

--- kernel/init/multiboot.asm
+++ kernel/init/multiboot.asm
@@ -28,6 +28,21 @@ HEADER_FLAG_default equ HEADER_FLAG_align | HEADER_FLAG_memory_map | HEADER_FLAG
 
 HEADER_CHECKSUM equ -(HEADER_FLAG_default + HEADER_MAGIC)
 
+struc HEADER_multiboot
+  .flags resb 4
+  .unsupported0 resb 40
+  .mmap_length resb 4
+  .mmap_addr resb 4
+  .unsupported1 resb 36
+  .framebuffer_addr resb 8
+  .framebuffer_pitch resb 4
+  .framebuffer_width resb 4
+  .framebuffer_height resb 4
+  .framebuffe_bpp resb 1
+  .framebuffer_type resb 1
+  .color_info resb 6
+endstruc
+
 align 0x04
 header:
   dd HEADER_MAGIC
@@ -38,5 +53,9 @@ header:
   dd STATIC_EMPTY
   dd STATIC_EMPTY
   dd init
+  dd STATIC_EMPTY
+  dd KERNEL_VIDEO_WIDTH_pixel
+  dd KERNEL_VIDEO_HEIGHT_pixel
+  dd KERNEL_VIDEO_DEPTH_bit
 align 0x10
 header_end:
