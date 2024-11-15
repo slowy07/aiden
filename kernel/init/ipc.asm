e32c20db16339e6f08b8591238871afc6ff1bff5
slowy07 <slowy.arfy@proton.me>
2024-11-16 00:08:12 +0700

    chore: adding inter processing communication system
    Signed-off-by: slowy07 <slowy.arfy@proton.me>

---
+++ kernel/init/ipc.asm
@@ -0,0 +1,30 @@
+;MIT License
+;
+;Copyright (c) 2024 arfy slowy
+;
+;Permission is hereby granted, free of charge, to any person obtaining a copy
+;of this software and associated documentation files (the "Software"), to deal
+;in the Software without restriction, including without limitation the rights
+;to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+;copies of the Software, and to permit persons to whom the Software is
+;furnished to do so, subject to the following conditions:
+;
+;The above copyright notice and this permission notice shall be included in all
+;copies or substantial portions of the Software.
+;
+;THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;SOFTWARE.
+
+kernel_init_ipc:
+  mov ecx, KERNEL_IPC_SIZE_page_default
+  call kernel_memory_alloc
+  
+  call kernel_page_drain_few
+  mov qword [kernel_ipc_base_address], rdi
+  
+  mov qword [rdi + STATIC_STRUCTURE_BLOCK.link], rdi
